6472a5e38f4195d7354b176fce4d09e3390d26cc
Matt Dray <mwdray@gmail.com>
2022-09-07 14:16:01 +0100

    Add some notes, tweak eggs.R

--- eggs/eggs.R
+++ eggs/eggs.R
@@ -1,16 +1,20 @@
 # Extract and prepare egg statistics
-# Creates objects to pass to {a11ytables} in EARL presentation
 # Matt Dray, Aug 2022
+
+# This script reads in data from an ODS published on GOV.UK, rearranges it and
+# outputs some objects to be passed as arguments to create_a11ytables() in the 
+# presentation file (../index.qmd).
 
 
 # Attach packages ---------------------------------------------------------
 
-
-library(purrr)
-library(dplyr)
-library(tidyr)
-library(stringr)
-library(a11ytables)
+suppressPackageStartupMessages({
+  library(purrr)
+  library(dplyr)
+  library(tidyr)
+  library(stringr)
+  library(a11ytables)
+})
 
 
 # Extract data ------------------------------------------------------------
@@ -105,7 +109,7 @@ pivot_features <- tribble(
 )
 
 # Map over each set of features to pivot longer and correct numeric rounding
-tables_list <- pmap(pivot_features, lengthen_tables) |> 
+tables_list <- suppressWarnings(pmap(pivot_features, lengthen_tables)) |> 
   map(as.data.frame) |>  # convert from tibbles for demo purposes
   set_names(table_names)
 
@@ -117,42 +121,42 @@ tables_list <- pmap(pivot_features, lengthen_tables) |>
 # alternating between subsection titles and subsection bodies)
 cover_df <- tribble(
   ~"subsection_title", ~"subsection_body",
-  "Purpose", "This publication gives quarterly information on egg production, usage and prices. This includes UK egg packing station throughput by country and egg production system (intensive, barn, free range, organic) and prices paid by UK egg packers to producers. The information about egg usage includes the number of eggs bought by UK egg processors and the quantity of egg products they produce. Monthly information about trade in eggs and egg products is also included.",
-  "Shorthand", "All sheets in this workbook use shorthand: [z] means 'not applicable' because data was not collected and [c] means the value was suppressed for confidentiality reasons.",
-  "Methodology", paste0("\nDefra runs a quarterly survey of registered UK egg packing stations. It is a voluntary sample survey of 27 respondents that collects information on throughput by production type and prices of graded eggs and sales of ungraded eggs. The response rate is typically 100 per cent and the survey accounts for 75 per cent of eggs packed in the UK. The survey figures are raised up to give UK estimates using information on the number of commercial laying hens, average egg yields, average mortality rates, the proportion of UK eggs that go through packing stations. Throughput by egg type for packing stations not surveyed is calculated using data provided by packing stations responding to the survey.  The raised figures are published in this statistics notice and the associated datasets. The figures in this notice therefore represent all Class A eggs passed over a grader in the UK, including seconds. The prices obtained on the survey are weighted according to the volume of eggs packed by each packing station to obtained average prices for the UK. From 2012, prices include any bonus payments paid to producers.",
-                        "\n\nIn tables that show numbers of eggs the units used are 'thousand cases'. There are 360 eggs in one case.",
-                        "\n\nThe data are subject to a variety of validation checks which identify inconsistencies in the data. All data are cleaned prior to publication.",
-                        "\n\nThe percentage changes shown are calculated using unrounded figures. Thus any percentage changes calculated using the published (rounded) figures may not equate exactly with the changes shown.",
-                        "\n\nThroughput was previously published in cases. To aid users who wish to convert this data from dozens into cases; there are 360 eggs or 30 dozens in a case.",
-                        "\n\nThese data represent all Class A eggs passed over a grader, including seconds."),
-  "Revisions policy", paste0("Figures in this dataset are provisional and subject to revision. We will provide information about any revisions we make to previously published information in this dataset, and the associated statistics notice. Revisions could occur for various reasons including:",
-                             "if we have not received survey data from respondents we make an estimate based on their previous returns (these estimates will be replaced with actual survey data when it is received)",
-                             "; if survey respondents occasionally supply amended figures for previous periods",
-                             "; or if we have revised the methodology used to raise the survey data to give UK totals."),
-  "Data users", paste("Representatives of the egg and poultry industry are also major users of the data. The data on egg production volumes and egg type are the key sector indicators for the British Egg Industry Council (BEIC) as they reflect the size of the national laying flock. The Home Grown Cereals Authority (HGCA), part of the Agricultural and Horticultural Development Board, rely on egg production data as a good indicator of the commercial layer flock and associated feed demand and hence grain usage by the sector.",
-                      "Our statistics are also often heavily referenced in industry publications such as 'Poultry World'."),
-  "Contact", "Firstname Lastname, +44 (0) 1234 567 890, firstname.lastname@department.gov.uk"
+  "Purpose",           "This publication gives quarterly information on egg production, usage and prices. This includes UK egg packing station throughput by country and egg production system (intensive, barn, free range, organic) and prices paid by UK egg packers to producers. The information about egg usage includes the number of eggs bought by UK egg processors and the quantity of egg products they produce. Monthly information about trade in eggs and egg products is also included.",
+  "Shorthand",         "All sheets in this workbook use shorthand: [z] means 'not applicable' because data was not collected and [c] means the value was suppressed for confidentiality reasons.",
+  "Methodology",       paste0("\nDefra runs a quarterly survey of registered UK egg packing stations. It is a voluntary sample survey of 27 respondents that collects information on throughput by production type and prices of graded eggs and sales of ungraded eggs. The response rate is typically 100 per cent and the survey accounts for 75 per cent of eggs packed in the UK. The survey figures are raised up to give UK estimates using information on the number of commercial laying hens, average egg yields, average mortality rates, the proportion of UK eggs that go through packing stations. Throughput by egg type for packing stations not surveyed is calculated using data provided by packing stations responding to the survey.  The raised figures are published in this statistics notice and the associated datasets. The figures in this notice therefore represent all Class A eggs passed over a grader in the UK, including seconds. The prices obtained on the survey are weighted according to the volume of eggs packed by each packing station to obtained average prices for the UK. From 2012, prices include any bonus payments paid to producers.",
+                              "\n\nIn tables that show numbers of eggs the units used are 'thousand cases'. There are 360 eggs in one case.",
+                              "\n\nThe data are subject to a variety of validation checks which identify inconsistencies in the data. All data are cleaned prior to publication.",
+                              "\n\nThe percentage changes shown are calculated using unrounded figures. Thus any percentage changes calculated using the published (rounded) figures may not equate exactly with the changes shown.",
+                              "\n\nThroughput was previously published in cases. To aid users who wish to convert this data from dozens into cases; there are 360 eggs or 30 dozens in a case.",
+                              "\n\nThese data represent all Class A eggs passed over a grader, including seconds."),
+  "Revisions policy",  paste0("Figures in this dataset are provisional and subject to revision. We will provide information about any revisions we make to previously published information in this dataset, and the associated statistics notice. Revisions could occur for various reasons including:",
+                              "if we have not received survey data from respondents we make an estimate based on their previous returns (these estimates will be replaced with actual survey data when it is received)",
+                              "; if survey respondents occasionally supply amended figures for previous periods",
+                              "; or if we have revised the methodology used to raise the survey data to give UK totals."),
+  "Data users",        paste("Representatives of the egg and poultry industry are also major users of the data. The data on egg production volumes and egg type are the key sector indicators for the British Egg Industry Council (BEIC) as they reflect the size of the national laying flock. The Home Grown Cereals Authority (HGCA), part of the Agricultural and Horticultural Development Board, rely on egg production data as a good indicator of the commercial layer flock and associated feed demand and hence grain usage by the sector.",
+                             "Our statistics are also often heavily referenced in industry publications such as 'Poultry World'."),
+  "Contact",           "Firstname Lastname, +44 (0) 1234 567 890, firstname.lastname@department.gov.uk"
 )
 
 # Table of contents
 contents_df <- tribble(
   ~"Tab name", ~"Sheet title",
-  "Notes", "Notes",
-  "Table_1", "Throughput by country, quarterly",
-  "Table_2", "Throughput by system, quarterly",
-  "Table_3", "Average packer to producer prices, quarterly",
-  "Table_4", "Throughput by country, annual",
-  "Table_5", "Throughput by system, annual",
-  "Table_6", "Average packer to producer prices, annual"
+  "Notes",     "Notes",
+  "Table_1",   "Throughput by country, quarterly",
+  "Table_2",   "Throughput by system, quarterly",
+  "Table_3",   "Average packer to producer prices, quarterly",
+  "Table_4",   "Throughput by country, annual",
+  "Table_5",   "Throughput by system, annual",
+  "Table_6",   "Average packer to producer prices, annual"
 )
 
 # Table of notes (notes in square brackets as per the guidance)
 notes_df <- tribble(
   ~"Note number", ~"Note text",
-  "[note 1]", "All eggs from free range and organic flocks have been re-labelled as barn eggs since 21st March 2022 due to avian influenza restrictions.",
-  "[note 2]", "From Q1 2006 the average UK All Types Price includes Organic eggs.",
-  "[note 3]", "Prices up to and including 2011 are a period end price and exclude bonus payments.",
-  "[note 4]", "From Q1 2012 prices shown are based on an average across the period and include bonus payments."
+  "[note 1]",     "All eggs from free range and organic flocks have been re-labelled as barn eggs since 21st March 2022 due to avian influenza restrictions.",
+  "[note 2]",     "From Q1 2006 the average UK All Types Price includes Organic eggs.",
+  "[note 3]",     "Prices up to and including 2011 are a period end price and exclude bonus payments.",
+  "[note 4]",     "From Q1 2012 prices shown are based on an average across the period and include bonus payments."
 )
 
 # Combine into a list
